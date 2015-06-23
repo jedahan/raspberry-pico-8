4b0830d9d0d7e43c6bdfea60348b88863f5d84d6
Jonathan Dahan <jonathan@jedahan.com>
2015-06-23 16:41:24 +0900

    more automation, less interaction

--- creating-pi-images.bash
+++ creating-pi-images.bash
@@ -11,7 +11,7 @@ PIHO_IMAGE_NAME="minimal" && piho ls | grep "$PIHO_IMAGE_NAME" >/dev/null || {
   piho create "$PIHO_IMAGE_NAME"
   piho run "$PIHO_IMAGE_NAME" bash -c "apt-get remove -y --purge scratch pypy-upstream sonic-pi freepats libraspberrypi-doc oracle-java8-jdk wolfram-engine && apt-get autoremove -y"
   piho run "$PIHO_IMAGE_NAME" apt-get update
-  piho run "$PIHO_IMAGE_NAME" apt-get upgrade
+  piho run "$PIHO_IMAGE_NAME" apt-get upgrade -y
 }
 
 PIHO_OLD_IMAGE_NAME="$PIHO_IMAGE_NAME"
@@ -21,11 +21,12 @@ PIHO_IMAGE_NAME="sdl2" && piho ls | grep "$PIHO_IMAGE_NAME" >/dev/null || {
   SDL2_SRC="SDL2-2.0.3.tar.gz"
 # instructions ripped from https://solarianprogrammer.com/2015/01/22/raspberry-pi-raspbian-getting-started-sdl-2/
   piho run "$PIHO_IMAGE_NAME" apt-get install -y build-essential libfreeimage-dev libopenal-dev libpango1.0-dev libsndfile-dev libudev-dev libasound2-dev libjpeg8-dev libtiff5-dev libwebp-dev automake
-  curl -OL https://www.libsdl.org/release/"$SDL2_SRC" && piho copy "$PIHO_IMAGE_NAME" "$SDL2_SRC" /home/pi
-  piho run "$PIHO_IMAGE_NAME" tar xf "$SDL2_SRC" && mkdir "${SDL2_SRC%%.tar.gz}/build"
+  curl -OL https://www.libsdl.org/release/"$SDL2_SRC" && piho copy "$PIHO_IMAGE_NAME" "$SDL2_SRC" "$WORKDIR"
+  WORKDIR=/home/pi
+  piho run "$PIHO_IMAGE_NAME" cd "$WORKDIR" && tar xf "$SDL2_SRC" && mkdir "${SDL2_SRC%%.tar.gz}/build"
 # build with OpenGL ES support only (fullscreen)
-  piho run "$PIHO_IMAGE_NAME" cd /home/pi/"${SDL2_SRC%%.tar.gz}"/build && ../configure --host=armv7l-raspberry-linux-gnueabihf --disable-pulseaudio --disable-esd --disable-video-mir --disable-video-wayland --disable-video-x11 --disable-video-opengl && make -j4 && make install
-  piho run "$PIHO_IMAGE_NAME" rm -rf /home/pi/"${SDL2_SRC%%.tar.gz}"
+  piho run "$PIHO_IMAGE_NAME" cd "$WORKDIR"/"${SDL2_SRC%%.tar.gz}"/build && ../configure --host=armv7l-raspberry-linux-gnueabihf --disable-pulseaudio --disable-esd --disable-video-mir --disable-video-wayland --disable-video-x11 --disable-video-opengl && make -j4 && make install
+  piho run "$PIHO_IMAGE_NAME" rm -rf "$WORKDIR"/"${SDL2_SRC%%.tar.gz}"
 }
 
 PIHO_OLD_IMAGE_NAME="$PIHO_IMAGE_NAME"

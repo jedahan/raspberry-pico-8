98cef003e9f4577e547b8db2ec4b1c536600bf3a
Jonathan Dahan <jonathan@jedahan.com>
2015-06-24 00:05:28 +0900

    split out some redundencies

--- sdl2_with_sdl2_image.bash
+++ sdl2_with_sdl2_image.bash
@@ -2,36 +2,43 @@
 set -e
 
 echo "You should read this script before running it"
-exit 0
+#exit 0
+
+PIHO_IMAGE_NAME="sdl2_with_sdl2_image"
+WORKDIR=/home/pi
+SDL2_SRC="SDL2-2.0.3.tar.gz"
+SDL2_IMAGE_SRC="SDL2_image-2.0.0.tar.gz"
+
+function pirun {
+  piho run $PIHO_IMAGE_NAME "$@"
+}
+function pibash {
+  piho run $PIHO_IMAGE_NAME bash -c "$@"
+}
 
 echo "Checking for and installing the latest version of piho"
 piho >/dev/null || bash <(curl https://piho.sh) && piho init
 
 echo "Creating a minimal raspbian image that can build from source"
-PIHO_IMAGE_NAME="sdl2_with_sdl2_image"
 piho create "$PIHO_IMAGE_NAME"
-piho run "$PIHO_IMAGE_NAME" bash -c "apt-get remove -y --purge scratch pypy-upstream sonic-pi freepats libraspberrypi-doc oracle-java8-jdk wolfram-engine && apt-get autoremove -y"
-piho run "$PIHO_IMAGE_NAME" apt-get update
-piho run "$PIHO_IMAGE_NAME" apt-get upgrade -y
-piho run "$PIHO_IMAGE_NAME" apt-get install -y build-essential libfreeimage-dev libopenal-dev libpango1.0-dev libsndfile-dev libudev-dev libasound2-dev libjpeg8-dev libtiff5-dev libwebp-dev automake
+pirun apt-get remove -y --purge scratch pypy-upstream sonic-pi freepats libraspberrypi-doc oracle-java8-jdk wolfram-engine
+pirun apt-get autoremove -y
+pirun apt-get update
+pirun apt-get upgrade -y
+pirun apt-get install -y build-essential libfreeimage-dev libopenal-dev libpango1.0-dev libsndfile-dev libudev-dev libasound2-dev libjpeg8-dev libtiff5-dev libwebp-dev automake
 
-echo "Installing sdl2"
-WORKDIR=/home/pi
-SDL2_SRC="SDL2-2.0.3.tar.gz"
+echo "Installing sdl2 with OpenGL ES support only (fullscreen)"
 curl -OL https://www.libsdl.org/release/"$SDL2_SRC" && piho copy "$PIHO_IMAGE_NAME" "$SDL2_SRC" "$WORKDIR"
-piho run "$PIHO_IMAGE_NAME" bash -c "cd $WORKDIR && tar xf $SDL2_SRC && mkdir ${SDL2_SRC%%.tar.gz}/build"
-# build with OpenGL ES support only (fullscreen)
-piho run "$PIHO_IMAGE_NAME" bash -c "cd ${WORKDIR}/${SDL2_SRC%%.tar.gz}/build && ../configure --host=armv7l-raspberry-linux-gnueabihf --disable-pulseaudio --disable-esd --disable-video-mir --disable-video-wayland --disable-video-x11 --disable-video-opengl && make -j4 && make install"
-piho run "$PIHO_IMAGE_NAME" bash -c "rm -rf ${WORKDIR}/${SDL2_SRC%%.tar.gz}"
+pibash cd $WORKDIR && tar xf $SDL2_SRC && mkdir ${SDL2_SRC%%.tar.gz}/build
+pibash cd ${WORKDIR}/${SDL2_SRC%%.tar.gz}/build && ../configure --host=armv7l-raspberry-linux-gnueabihf --disable-pulseaudio --disable-esd --disable-video-mir --disable-video-wayland --disable-video-x11 --disable-video-opengl && make -j4 && make install
+pirun rm -rf ${WORKDIR}/${SDL2_SRC%%.tar.gz}
 
 echo "Installing sdl2_image"
-WORKDIR=/home/pi
-SDL2_IMAGE_SRC="SDL2_image-2.0.0.tar.gz"
 curl -OL http://www.libsdl.org/projects/SDL_image/release/$SDL2_IMAGE_SRC && piho copy "$PIHO_IMAGE_NAME" "$SDL2_IMAGE_SRC" "$WORKDIR"
-piho run "$PIHO_IMAGE_NAME" bash -c "cd $WORKDIR && tar xf $SDL2_IMAGE_SRC && mkdir ${SDL2_IMAGE_SRC%%.tar.gz}/build"
-piho run "$PIHO_IMAGE_NAME" bash -c "cd ${WORKDIR}/${SDL2_IMAGE_SRC%%.tar.gz}/build && ../configure && make -j4 && make install"
-piho run "$PIHO_IMAGE_NAME" bash -c "rm -rf ${WORKDIR}/${SDL2_IMAGE_SRC%%.tar.gz}"
+pibash cd $WORKDIR && tar xf $SDL2_IMAGE_SRC && mkdir ${SDL2_IMAGE_SRC%%.tar.gz}/build
+pibash cd ${WORKDIR}/${SDL2_IMAGE_SRC%%.tar.gz}/build && ../configure && make -j4 && make install
+pirun  rm -rf ${WORKDIR}/${SDL2_IMAGE_SRC%%.tar.gz}
 
 echo "Compiling and running test program"
 piho copy "$PIHO_IMAGE_NAME" sdl2_image_test.cpp img_test.png "$WORKDIR"
-piho run "$PIHO_IMAGE_NAME" bash -c "g++ -std=c++0x -Wall -pedantic ${WORKDIR}/sdl2_image_test.cpp -o sdl2_test `sdl2-config --cflags --libs` -lSDL2_image"
+pibash g++ -std=c++0x -Wall -pedantic ${WORKDIR}/sdl2_image_test.cpp -o sdl2_test `sdl2-config --cflags --libs` -lSDL2_image

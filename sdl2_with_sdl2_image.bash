835603feb77ab6babfd7b347ee5e11939f6d5518
Jonathan Dahan <jonathan@jedahan.com>
2015-06-28 23:16:14 +0900

    split into steps

--- sdl2_with_sdl2_image.bash
+++ sdl2_with_sdl2_image.bash
@@ -1,35 +1,43 @@
 #!/bin/bash
 set -e
 
-echo "You should read this script before running it"
-exit 0
+piho >/dev/null || ( echo "This script requires piho, install from https://github.com/xdissent/pihotenuse/" && exit ) 
 
-PIHO_IMAGE_NAME="sdl2_with_sdl2_image"
 WORKDIR=/home/pi
-SDL2_SRC="SDL2-2.0.3.tar.gz"
-SDL2_IMAGE_SRC="SDL2_image-2.0.0.tar.gz"
 
-echo "Checking for and installing the latest version of piho"
-piho >/dev/null || bash <(curl https://piho.sh) && piho init
+image="minimal"
+piho ls | grep $image >/dev/null && echo "$image found, going to next step" || (
+  echo "Creating a minimal raspbian image that can build from source"
+  piho ls | grep $image >/dev/null || piho create "$image"
+  piho run $image apt-get remove -y --purge scratch pypy-upstream sonic-pi freepats libraspberrypi-doc oracle-java8-jdk wolfram-engine
+  piho run $image apt-get autoremove -y
+  piho run $image apt-get update
+  piho run $image apt-get upgrade -y
+  piho run $image apt-get install -y build-essential libfreeimage-dev libopenal-dev libpango1.0-dev libsndfile-dev libudev-dev libasound2-dev libjpeg8-dev libtiff5-dev libwebp-dev automake
+)
 
-echo "Creating a minimal raspbian image that can build from source"
-piho ls | grep $PIHO_IMAGE_NAME >/dev/null || piho create "$PIHO_IMAGE_NAME"
-piho run $PIHO_IMAGE_NAME apt-get remove -y --purge scratch pypy-upstream sonic-pi freepats libraspberrypi-doc oracle-java8-jdk wolfram-engine
-piho run $PIHO_IMAGE_NAME apt-get autoremove -y
-piho run $PIHO_IMAGE_NAME apt-get update
-piho run $PIHO_IMAGE_NAME apt-get upgrade -y
-piho run $PIHO_IMAGE_NAME apt-get install -y build-essential libfreeimage-dev libopenal-dev libpango1.0-dev libsndfile-dev libudev-dev libasound2-dev libjpeg8-dev libtiff5-dev libwebp-dev automake
+image_old=$image && image="sdl2"
+piho ls | grep $image >/dev/null && echo "$image found, going to next step" || (
+  echo "Installing sdl2 with OpenGL ES support only (fullscreen)"
+  piho clone $old_image $image
+  local SDL2_SRC="SDL2-2.0.3.tar.gz"
+  [[ -f $SDL2_SRC ]] || curl -OL https://www.libsdl.org/release/"$SDL2_SRC" && piho copy "$image" "$SDL2_SRC" "$WORKDIR"
+  piho run $image bash -c "cd $WORKDIR && tar xf $SDL2_SRC && cd ${SDL2_SRC%%.tar.gz} && mkdir -p build && cd build && ../configure --host=armv7l-raspberry-linux-gnueabihf --disable-pulseaudio --disable-esd --disable-video-mir --disable-video-wayland --disable-video-x11 --disable-video-opengl && make -j4 && make install"
+  piho run $image rm -rf ${WORKDIR}/${SDL2_SRC%%.tar.gz}
+)
 
-echo "Installing sdl2 with OpenGL ES support only (fullscreen)"
-[[ -f $SDL2_SRC ]] || curl -OL https://www.libsdl.org/release/"$SDL2_SRC" && piho copy "$PIHO_IMAGE_NAME" "$SDL2_SRC" "$WORKDIR"
-piho run $PIHO_IMAGE_NAME bash -c "cd $WORKDIR && tar xf $SDL2_SRC && cd ${SDL2_SRC%%.tar.gz} && mkdir -p build && cd build && ../configure --host=armv7l-raspberry-linux-gnueabihf --disable-pulseaudio --disable-esd --disable-video-mir --disable-video-wayland --disable-video-x11 --disable-video-opengl && make -j4 && make install"
-piho run $PIHO_IMAGE_NAME rm -rf ${WORKDIR}/${SDL2_SRC%%.tar.gz}
+image_old=$image && image="sdl2_image"
+piho ls | grep $image >/dev/null && echo "$image found, going to net step" || (
+  echo "Installing sdl2_image"
+  piho clone $old_image $image
+  local SDL2_IMAGE_SRC="SDL2_image-2.0.0.tar.gz"
+  [[ -f $SDL2_IMAGE_SRC ]] || ( curl -OL http://www.libsdl.org/projects/SDL_image/release/$SDL2_IMAGE_SRC && piho copy "$image" "$SDL2_IMAGE_SRC" "$WORKDIR" )
+  piho run $image bash -c "cd ${WORKDIR} && tar xf $SDL2_IMAGE_SRC && cd ${SDL2_IMAGE_SRC%%.tar.gz} && mkdir -p build && cd build && ../configure && make -j4 && make install"
+  piho run $image rm -rf ${WORKDIR}/${SDL2_IMAGE_SRC%%.tar.gz}
+)
 
-echo "Installing sdl2_image"
-[[ -f $SDL2_IMAGE_SRC ]] || ( curl -OL http://www.libsdl.org/projects/SDL_image/release/$SDL2_IMAGE_SRC && piho copy "$PIHO_IMAGE_NAME" "$SDL2_IMAGE_SRC" "$WORKDIR" )
-piho run $PIHO_IMAGE_NAME bash -c "cd ${WORKDIR} && tar xf $SDL2_IMAGE_SRC && cd ${SDL2_IMAGE_SRC%%.tar.gz} && mkdir -p build && cd build && ../configure && make -j4 && make install"
-piho run $PIHO_IMAGE_NAME rm -rf ${WORKDIR}/${SDL2_IMAGE_SRC%%.tar.gz}
-
-echo "Compiling and running test program"
-piho copy "$PIHO_IMAGE_NAME" `pwd`/sdl2_image_test.cpp `pwd`/img_test.png "$WORKDIR"
-piho run $PIHO_IMAGE_NAME bash -c 'cd /home/pi && g++ -std=c++0x -Wall -pedantic sdl2_image_test.cpp -o sdl2_test `sdl2-config --cflags --libs` -lSDL2_image'
+image_old=$image && image="pico-8"
+echo "recompiling and running test program"
+piho rm $image && piho clone $old_image $image
+piho copy "$image" `pwd`/sdl2_image_test.cpp `pwd`/img_test.png "$WORKDIR"
+piho run $image bash -c 'cd /home/pi && g++ -std=c++0x -Wall -pedantic sdl2_image_test.cpp -o sdl2_test `sdl2-config --cflags --libs` -lSDL2_image'

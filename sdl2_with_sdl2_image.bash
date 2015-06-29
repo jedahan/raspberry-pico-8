fe9811de6c6522ee0ede1370983a4cdcc43b3fd0
Jonathan Dahan <jonathan@jedahan.com>
2015-06-29 17:16:13 +0900

    sdl2_image and sdl2 merge

--- sdl2_with_sdl2_image.bash
+++ sdl2_with_sdl2_image.bash
@@ -18,19 +18,15 @@ piho ls | grep -q ^$image\$ && echo "[1/4] ${image}: found, skipping creation" |
 
 image_old=$image; image="sdl2"
 piho ls | grep -q ^$image\$ && echo "[2/4] $image: found, skipping creation" || {
-  echo "[2/4] $image: OpenGL ES-only version of SDL2 (v2.0.3)"
+  echo "[2/4] $image: OpenGL ES-only version of SDL2 (v2.0.3) + SDL2_image"
   piho clone "$image_old" "$image"
   sdl2_src="SDL2-2.0.3.tar.gz"
   [[ -f $sdl2_src ]] || curl -OL https://www.libsdl.org/release/"$sdl2_src"
   piho copy "$image" "$sdl2_src" "$workdir"
   piho run "$image" bash -c "cd $workdir && tar xf $sdl2_src && cd ${sdl2_src%%.tar.gz} && mkdir -p build && cd build && ../configure --host=armv7l-raspberry-linux-gnueabihf --disable-pulseaudio --disable-esd --disable-video-mir --disable-video-wayland --disable-video-x11 --disable-video-opengl && make -j4 && make install"
   piho run "$image" rm -rf ${workdir}/${sdl2_src%%.tar.gz}
-}
 
-image_old=$image; image="sdl2_image"
-piho ls | grep -q ^$image\$ && echo "[3/4] $image: found, skipping creation" || {
   echo "[3/4] $image: Adding sdl2_image"
-  piho clone "$image_old" "$image"
   sdl2_image_src="SDL2_image-2.0.0.tar.gz"
   [[ -f $sdl2_src ]] || curl -OL http://www.libsdl.org/projects/SDL_image/release/$sdl2_image_src
   piho copy "$image" "$sdl2_image_src" "$workdir"

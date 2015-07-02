51a397d2f331921d009c228c9343dcd3b7c92c19
Jonathan Dahan <jonathan@jonathan.is>
2015-07-02 16:32:22 +0900

    dont remove interim images

--- sdl2_with_sdl2_image.bash
+++ sdl2_with_sdl2_image.bash
@@ -6,8 +6,8 @@ piho >/dev/null || { echo "This script requires piho, install from https://githu
 workdir=/home/pi
 
 image="minimal"
-piho ls | grep -q ^$image\$ && echo "[2/5] ${image}: found, skipping creation" || {
-  echo "[0/5] $image: Updating raspbian and removing cruft"
+piho ls | grep -q ^$image\$ && echo "[$image] found, skipping creation" || {
+  echo "[$image] Updating raspbian and removing cruft"
   piho create "$image"
   piho run "$image" apt-get remove -y --purge scratch pypy-upstream sonic-pi freepats libraspberrypi-doc oracle-java8-jdk wolfram-engine
   piho run "$image" apt-get autoremove -y
@@ -15,7 +15,7 @@ piho ls | grep -q ^$image\$ && echo "[2/5] ${image}: found, skipping creation" |
   piho run "$image" apt-get upgrade -y
   piho run "$image" apt-get install -y build-essential libfreeimage-dev libopenal-dev libpango1.0-dev libsndfile-dev libudev-dev libasound2-dev libjpeg8-dev libtiff5-dev libwebp-dev automake
 
-  echo "[1/5] $image: configuring"
+  echo "[$image] configuring"
   # set locale to en_US.UTF-8
   piho r "$image" bash -c 'echo $0 $1 > /etc/locale.gen &&
     DEBIAN_FRONTEND=noninteractive dpkg-reconfigure locales' en_US.UTF-8 UTF-8
@@ -37,24 +37,24 @@ piho ls | grep -q ^$image\$ && echo "[2/5] ${image}: found, skipping creation" |
 
 image_old=$image; image="sdl2"
 piho ls | grep -q ^$image\$ && echo "[3/5] $image: found, skipping creation" || {
-  echo "[3/5] $image: OpenGL ES-only version of SDL2 (v2.0.3) + SDL2_image"
+  echo "[$image] OpenGL ES-only version of SDL2 (v2.0.3) + SDL2_image"
   piho clone "$image_old" "$image"
   sdl2_src="SDL2-2.0.3.tar.gz"
   [[ -f $sdl2_src ]] || curl -OL https://www.libsdl.org/release/"$sdl2_src"
-  piho copy "$image" "$sdl2_src" "$workdir" || piho rm "$image"
-  piho run "$image" bash -c "cd $workdir && tar xf $sdl2_src && cd ${sdl2_src%%.tar.gz} && mkdir -p build && cd build && ../configure --host=armv7l-raspberry-linux-gnueabihf --disable-pulseaudio --disable-esd --disable-video-mir --disable-video-wayland --disable-video-x11 --disable-video-opengl && make -j4 && make install" || piho rm "$image"
+  piho copy "$image" "$sdl2_src" "$workdir"
+  piho run "$image" bash -c "cd $workdir && tar xf $sdl2_src && cd ${sdl2_src%%.tar.gz} && mkdir -p build && cd build && ../configure --host=armv7l-raspberry-linux-gnueabihf --disable-pulseaudio --disable-esd --disable-video-mir --disable-video-wayland --disable-video-x11 --disable-video-opengl && make -j4 && make install"
   piho run "$image" rm -rf ${workdir}/${sdl2_src%%.tar.gz}
 
-  echo "[4/5] $image: Adding sdl2_image"
+  echo "[$image] Adding sdl2_image"
   sdl2_image_src="SDL2_image-2.0.0.tar.gz"
   [[ -f $sdl2_src ]] || curl -OL http://www.libsdl.org/projects/SDL_image/release/$sdl2_image_src
   piho copy "$image" "$sdl2_image_src" "$workdir"
-  piho run "$image" bash -c "cd ${workdir} && tar xf $sdl2_image_src && cd ${sdl2_image_src%%.tar.gz} && mkdir -p build && cd build && ../configure && make -j4 && make install" || piho rm "$image"
+  piho run "$image" bash -c "cd ${workdir} && tar xf $sdl2_image_src && cd ${sdl2_image_src%%.tar.gz} && mkdir -p build && cd build && ../configure && make -j4 && make install"
   piho run "$image" rm -rf "$workdir"/"${sdl2_image_src%%.tar.gz}"
 }
 
 image_old=$image; image="pico8"
-echo "[5/5] $image: compiling program"
+echo "[$image] compiling program"
 piho ls | grep -q ^$image\$ && piho rm "$image"
 piho clone "$image_old" "$image"
 piho copy "$image" "$PWD"/sdl2_image_test.cpp "$PWD"/img_test.png "$workdir"
